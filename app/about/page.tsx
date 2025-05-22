b0c295f73218ca2503b1d4b2dbcd8b28407b41e7
Enoch Oluwajoba <54731341+Imisioluwa3@users.noreply.github.com>
2025-05-22 15:09:47 +0100

    Switching from React to Vanilla Js

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-const About= () => {
-    return ( 
-        <div>
-            <h1>About Empire Solicitor</h1>
-        </div>
-     );
-}
- 
-export default About
--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => {
-    return ( 
-        <div className="footer">Copyright @ 2025 Empire Solicitor</div>
-     );
-}
- 
-export default Footer;
